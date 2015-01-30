af7491c68321c89e21f64257baae386a63b6aff9
arpanmukherjee1 <arpanmukherjee1@gmail.com>
2015-01-30 23:17:36 +0530

    first ML project checkin

---
+++ SigmoidFunction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SigmoidFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
